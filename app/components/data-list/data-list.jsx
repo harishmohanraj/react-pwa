2d266dfe9e1bd4bb416e3008f3962ab5de87a553
Harish Mohan Raj <hraj@sapient.com>
2016-09-19 19:10:29 +0100

    progressBar added

--- app/components/data-list/data-list.jsx
+++ app/components/data-list/data-list.jsx
@@ -6,6 +6,10 @@ require('./data-list.scss');
 export default class DataList extends React.Component {
 
     returnIndividualList(list,i){
+        const widthPercentage = isNaN(Math.round(list[8]/list[7]*100)) ? '0' : Math.round(list[8]/list[7]*100) ;
+        const progressBar = {
+            width : `${widthPercentage}%`
+        };
         return(
             <ul key={i} className="data-row">
                 <li className="data-column">{++i}</li>
@@ -16,8 +20,8 @@ export default class DataList extends React.Component {
                 <li className="data-column">
                 <div className="progress-bar horizontal">
                     <div className="progress-track">
-                        <span className="progress-info">100%</span>
-                        <div className="progress-fill"></div>
+                        <span className="progress-info">{`${widthPercentage}%`}</span>
+                        <div className="progress-fill" style={progressBar} ></div>
                     </div>
                 </div>
                 </li>

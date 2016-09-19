870c7d34ebeac26458669bfa9dfce261b6f5afd2
Harish Mohan Raj <hraj@sapient.com>
2016-09-19 20:12:39 +0100

    dynamic route names added

--- app/components/sidebar/sidebar.jsx
+++ app/components/sidebar/sidebar.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Config from '../../config/config.js';
 
 require('./sidebar.scss');
 
 export default class SideBar extends React.Component{
+    
+    constructor(){
+        super();
+        this.generateLists = this.generateLists.bind(this);
+    }
+    
+    generateLists(){
+        return Config.routes.map(list => this.returnListItem(list))    
+    }
+
+    returnListItem(list){
+        return <li><a href="#">{list.name}</a></li>
+    }
+
     render(){
         return(
             <aside className="side-bar">
                 <a className="site-logo"><img src="app/assets/site-logo.png" alt="My India logo"/></a>
                 <nav className="main-nav">
                     <ul>
-                        <li><a href="#">Test</a></li>
-                        <li><a href="#">Test</a></li>
-                        <li><a href="#">Test</a></li>
-                        <li><a href="#">Test</a></li>
+                        {this.generateLists()}
                     </ul>
                 </nav>
             </aside>
